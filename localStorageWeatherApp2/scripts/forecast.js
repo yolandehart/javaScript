55f4c9866572e598988d54200280ed192c4d8792
yolande hart <yolande.hart@icloud.com>
2020-11-10 12:48:32 +0700

    localStorageWeatherApp1

---
+++ localStorageWeatherApp2/scripts/forecast.js
@@ -0,0 +1,26 @@
+class Forecast {
+  constructor(){
+    this.key = 'l1AjA1Q6CaC3rQLY2MsNIRQhIoJpFxc9';;
+    
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  }
+  async updateCity(city){
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getCity(city){
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+}
